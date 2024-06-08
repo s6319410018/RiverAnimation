ad9b8aef1c2a35fc0aa85e61330661d1a559b5d1
s6319410018 <s6319410018@sau.ac.th>
2024-06-08 09:03:42 +0700

    v.3 by 6319410018

--- lib/Authentication/login_screen.dart
+++ lib/Authentication/login_screen.dart
@@ -266,6 +266,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                 ),
+         
                 SizedBox(
                   height: 300,
                 )
--- lib/Screens/Mobile_body.dart
+++ lib/Screens/Mobile_body.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rive/rive.dart';
+import 'package:riveanimationapp/Authentication/validate.dart';
 
 class MoBileBodyScreen extends StatefulWidget {
   const MoBileBodyScreen({Key? key}) : super(key: key);
@@ -56,52 +59,241 @@ class _MoBileBodyScreenState extends State<MoBileBodyScreen> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: Container(
-                height: height,
-                width: screenWidth,
-                color: Colors.deepPurple[400],
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 15),
-                  child: RiveAnimation.asset(
-                    "assets/images/animated_login_character.riv",
-                    stateMachines: const ["Login Machine"],
-                    onInit: (artboard) {
-                      final controller = StateMachineController.fromArtboard(
-                        artboard,
-                        "Login Machine",
-                      );
-                      if (controller != null) {
-                        stateMachineController = controller;
-                        artboard.addController(controller);
-                        isChecking = stateMachineController
-                            ?.findInput<bool>("isChecking");
-                        trigFail =
-                            stateMachineController?.findInput<bool>("trigFail");
-                        trigSuccess = stateMachineController
-                            ?.findInput<bool>("trigSuccess");
-                        isHandsUp = stateMachineController
-                            ?.findInput<bool>("isHandsUp");
-                        numLook = stateMachineController
-                            ?.findSMI<SMINumber>("numLook");
-                      }
-                    },
-                  ),
+            Container(
+              height: MediaQuery.of(context).size.height * 1,
+              color: Colors.blue.shade100,
+              alignment: Alignment.center,
+              width: double.infinity,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                          top: 0, left: 0, right: 0, bottom: 10),
+                      child: Container(
+                        height: height,
+                        width: screenWidth,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(30),
+                              bottomRight: Radius.circular(30),
+                              topLeft: Radius.circular(0),
+                              topRight: Radius.circular(0)),
+                          color: Colors.white,
+                          shape: BoxShape.rectangle,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 15),
+                          child: RiveAnimation.asset(
+                            "assets/images/animated_login_character.riv",
+                            stateMachines: const ["Login Machine"],
+                            onInit: (artboard) {
+                              final controller =
+                                  StateMachineController.fromArtboard(
+                                artboard,
+                                "Login Machine",
+                              );
+                              if (controller != null) {
+                                stateMachineController = controller;
+                                artboard.addController(controller);
+                                isChecking = stateMachineController
+                                    ?.findInput<bool>("isChecking");
+                                trigFail = stateMachineController
+                                    ?.findInput<bool>("trigFail");
+                                trigSuccess = stateMachineController
+                                    ?.findInput<bool>("trigSuccess");
+                                isHandsUp = stateMachineController
+                                    ?.findInput<bool>("isHandsUp");
+                                numLook = stateMachineController
+                                    ?.findSMI<SMINumber>("numLook");
+                              }
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Login",
+                      style: GoogleFonts.kanit(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          shadows: [
+                            Shadow(
+                              color: Colors.black,
+                              blurRadius: 0.45,
+                              offset: Offset(0.5, 0.9),
+                            )
+                          ]),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 10, left: 10),
+                      child: TextFormField(
+                        onTap: isCheckingField,
+                        onEditingComplete: () {
+                          isChecking?.change(false);
+                          FocusManager.instance.primaryFocus?.unfocus();
+                        },
+                        controller: _username,
+                        style: const TextStyle(fontSize: 15),
+                        cursorColor: Colors.red,
+                        decoration: InputDecoration(
+                          hintText: "Username",
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: const BorderSide(color: Colors.red),
+                          ),
+                        ),
+                        validator: (value) {
+                          setState(() {
+                            emailValid = AppRegex.isEmailValid(value ?? '');
+                            if (emailValid) {
+                              errorMessageEmail = '';
+                            } else {
+                              errorMessageEmail = 'Invalid email format';
+                            }
+                          });
+
+                          return null;
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      onTap: () {
+                        setState(() {
+                          if (_password.text.isEmpty) {
+                            isChecking?.change(true);
+                          } else {
+                            obscure = false;
+                            isHandsUp?.change(true);
+                          }
+                        });
+                      },
+                      onChanged: (value) {
+                        setState(() {
+                          passwordValid = isPasswordValid(value);
+                          obscure = false;
+                          isHandsUp?.change(true);
+                          if (passwordValid) {
+                            errorMessagePassword = '';
+                          } else {
+                            errorMessagePassword =
+                                'Password must contain at least 8 characters, one uppercase letter, and one symbol.';
+                          }
+                        });
+                      },
+                      onEditingComplete: () {
+                        isHandsUp?.change(false);
+                        isChecking?.change(false);
+                        FocusManager.instance.primaryFocus?.unfocus();
+                        setState(() {
+                          obscure = true;
+                          isHandsUp?.change(false);
+                        });
+                      },
+                      obscureText: obscure,
+                      controller: _password,
+                      style: const TextStyle(fontSize: 15),
+                      cursorColor: Colors.red,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: BorderSide.none,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: const BorderSide(color: Colors.red),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            obscure ? Icons.visibility_off : Icons.visibility,
+                            color: Colors.grey,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              obscure = !obscure;
+                              if (obscure == false) {
+                                isHandsUp?.change(true);
+                              } else {
+                                isHandsUp?.change(false);
+                              }
+                            });
+                          },
+                        ),
+                      ),
+                      validator: (value) {
+                        setState(() {
+                          passwordValid = isPasswordValid(value ?? '');
+                          if (passwordValid) {
+                            errorMessagePassword = '';
+                          }
+                        });
+
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 6,
+                    ),
+                    Text(
+                      errorMessageEmail.isNotEmpty
+                          ? errorMessageEmail
+                          : (errorMessagePassword.isNotEmpty
+                              ? errorMessagePassword
+                              : ''),
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 12,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50, // Set the desired height
+                      child: CupertinoButton(
+                        color: Colors.blue.shade300,
+                        child: Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate() &&
+                              emailValid &&
+                              passwordValid) {
+                            isHandsUp?.change(false);
+                            isChecking?.change(false);
+                            numLook?.change(0);
+                            trigSuccess?.change(true);
+                          } else {
+                            trigFail?.change(true);
+                          }
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            Container(
-              height: 40,
-              width: double.infinity,
-              color: Colors.deepPurple,
-              child: Text('data'),
-            ),
-            Container(
-              height: 10,
-              width: double.infinity,
-              color: Colors.deepPurple,
-              child: Text('data'),
             ),
           ],
         ),
